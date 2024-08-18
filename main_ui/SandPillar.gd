97ad46544c71b6491b40c4cb5b3dc663adcc2416
xingguix <xing_gui@outlook.com>
2024-08-19 00:21:21 +0800

    沙漏完成

--- main_ui/SandPillar.gd
+++ main_ui/SandPillar.gd
@@ -1,8 +1,9 @@
+@tool
 extends TextureRect
 
 func _process(delta):
 	var parent = get_parent()
 	pivot_offset = Vector2(size.x/2, parent.size.y)
-	var time = TimeTransfer.time
-	time = 30
-	size.y = (time / 120) * parent.size.y
+	if not Engine.is_editor_hint():
+		var time = TimeTransfer.time
+		scale.y = (float(time) / 120)
